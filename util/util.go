d34ae306e15d42b7d6e249209348aa36211f5761
H. Emre ARI <hemreari98@gmail.com>
2020-11-04 13:20:35 +0300

    add GetBodyRequest func unit test

--- util/util.go
+++ util/util.go
@@ -3,18 +3,18 @@ package util
 import (
 	"encoding/json"
 	"fmt"
-	"strings"
 	"net/url"
+	"strings"
 )
 
 //requestBody used for marshalling JSON data.
 type requestBody struct {
-	args    []string `json:"args"`
-	account string   `json:"account"`
+	Args    []interface{} `json:"args"`
+	Account string        `json:"account"`
 }
 
-//GetHttpGetUrl returns Http GET url for integration call.
-func GetHttpGetUrl(baseUrl, shortId, method, tag, account string, args []string) string {
+//GetHttpGetUrl returns Http GET url for contract call.
+func GetHttpGetUrl(baseUrl, shortId, method, tag, account string, args []interface{}) string {
 	reqUrl := baseUrl + "/int/" + strings.TrimSpace(shortId) + "/" + strings.TrimSpace(method)
 	//tag is not empty, include tag in the reqUrl
 	if strings.Compare(tag, "") != 0 {
@@ -28,23 +28,21 @@ func GetHttpGetUrl(baseUrl, shortId, method, tag, account string, args []string)
 		reqUrl += "?"
 		params.Add("account", account)
 	}
-
 	//args doesn't empty
 	if len(args) > 0 {
 		if strings.Compare(account, "") == 0 {
 			reqUrl += "?"
 		}
 		for _, elem := range args {
-			//TODO: check if args could take any integer value.
-			params.Add("args", elem)
+			params.Add("args", fmt.Sprintf("%v", elem))
 		}
 	}
 	return reqUrl + params.Encode()
 }
 
-//GetHttpPostUrl returns Http POST url for integration send.
+//GetHttpPostUrl returns Http POST url for contract send.
 func GetHttpPostUrl(baseUrl, shortId, method, tag string) string {
-	url := baseUrl + "/int/" + strings.TrimSpace(shortId) + "/" + strings.TrimSpace(method)
+	url := baseUrl + "int/" + strings.TrimSpace(shortId) + "/" + strings.TrimSpace(method)
 
 	if strings.Compare(tag, "") != 0 {
 		url += "/" + strings.TrimSpace(tag)
@@ -53,15 +51,19 @@ func GetHttpPostUrl(baseUrl, shortId, method, tag string) string {
 	return url
 }
 
-func GetBodyRequest(account string, args []string) ([]byte, error) {
-	//init requestBody struct
-	requestBodyStruct := requestBody{
-		args:    args,
-		account: account,
+//GetBodyRequest returns a JSON object that contains account and args.
+func GetBodyRequest(account string, args []interface{}) ([]byte, error) {
+	requestBodyStruct := requestBody{}
+	if args != nil {
+		//add args to struct slice
+		for _, elem := range args {
+			requestBodyStruct.Args = append(requestBodyStruct.Args, elem)
+		}
 	}
 
-	//TODO: is account & args is required.
-	//get request body in json
+	requestBodyStruct.Account = account
+
+	//parse struct to JSON
 	requestBody, err := json.Marshal(requestBodyStruct)
 	if err != nil {
 		return nil, fmt.Errorf("error while creating request body: %v", err)
--- util/util_test.go
+++ util/util_test.go
@@ -16,30 +16,29 @@ func TestGetHttpGetUrl(t *testing.T) {
 		method  string
 		tag     string
 		account string
-		args    []string
 	}{
 		//everything is present
-		{base_url+"/int/shortId1/method1/tag1?account=account1&args=arg1&args=arg2",
-			"shortId1", "method1", "tag1", "account1", []string{"arg1", "arg2"}},
+		{base_url + "/int/shortId1/method1/tag1?account=account1&args=1&args=2&args=%40&args=true",
+			"shortId1", "method1", "tag1", "account1"},
 		//tag is missing
-		{base_url+"/int/shortId2/method2?account=account2&args=arg1&args=arg2",
-			"shortId2", "method2", "", "account2", []string{"arg1", "arg2"}},
+		{base_url + "/int/shortId2/method2?account=account2&args=1&args=2&args=%40&args=true",
+			"shortId2", "method2", "", "account2"},
 		//account is missing
-		{base_url+"/int/shortId3/method3/tag3?args=arg1&args=arg2",
-			"shortId3", "method3", "tag3", "", []string{"arg1", "arg2"}},
+		{base_url + "/int/shortId3/method3/tag3?args=1&args=2&args=%40&args=true",
+			"shortId3", "method3", "tag3", ""},
 		//args is missing
-		{base_url+"/int/shortId4/method4/tag4?account=account4",
-			"shortId4", "method4", "tag4", "account4", nil},
+		{base_url + "/int/shortId4/method4/tag4?account=account4&args=1&args=2&args=%40&args=true",
+			"shortId4", "method4", "tag4", "account4"},
 		//all optional params are missing
-		{base_url+"/int/shortId5/method5",
-			"shortId5", "method5", "", "", nil},
+		{base_url + "/int/shortId5/method5?args=1&args=2&args=%40&args=true",
+			"shortId5", "method5", "", ""},
 		//tag and account are missing
-		{base_url+"/int/shortId6/method6?account=account6&args=arg1&args=arg2",
-			"shortId6", "method6", "", "account6", []string{"arg1", "arg2"}},
+		{base_url + "/int/shortId6/method6?account=account6&args=1&args=2&args=%40&args=true",
+			"shortId6", "method6", "", "account6"},
 	}
 
-	for _, table := range tables{
-		gotUrl := GetHttpGetUrl(base_url, table.shortId, table.method, table.tag, table.account, table.args)
+	for _, table := range tables {
+		gotUrl := GetHttpGetUrl(base_url, table.shortId, table.method, table.tag, table.account, []interface{}{"1", 2, "@", true})
 		if strings.Compare(gotUrl, table.url) != 0 {
 			t.Errorf("Got: '%s', Want: '%s'", gotUrl, table.url)
 		}
@@ -53,17 +52,17 @@ func TestGetHttpPostUrl(t *testing.T) {
 		method  string
 		tag     string
 	}{
-		{base_url+"/int/shortId1/method1/tag1",
+		{base_url + "/int/shortId1/method1/tag1",
 			"shortId1", "method1", "tag1"},
-		{base_url+"/int/shortId2/method2",
+		{base_url + "/int/shortId2/method2",
 			"shortId2", "method2", ""},
-		{base_url+"/int/shortId3/method3/tag3",
+		{base_url + "/int/shortId3/method3/tag3",
 			"shortId3", "method3", "tag3"},
-		{base_url+"/int/shortId4/method4/tag4",
+		{base_url + "/int/shortId4/method4/tag4",
 			"shortId4", "method4", "tag4"},
-		{base_url+"/int/shortId5/method5",
+		{base_url + "/int/shortId5/method5",
 			"shortId5", "method5", ""},
-		{base_url+"/int/shortId6/method6",
+		{base_url + "/int/shortId6/method6",
 			"shortId6", "method6", ""},
 	}
 
@@ -71,6 +70,30 @@ func TestGetHttpPostUrl(t *testing.T) {
 		gotUrl := GetHttpPostUrl(base_url, table.shortId, table.method, table.tag)
 		if strings.Compare(gotUrl, table.url) != 0 {
 			t.Errorf("Got: '%s', Want: '%s'", gotUrl, table.url)
+		}
+	}
+}
+
+func TestGetBodyRequest(t *testing.T) {
+	tables := []struct {
+		wantJson    string
+		account     string
+		argsInt     int
+		argsBool    bool
+		argsStr     string
+		argsSpecial string
+	}{
+		{"{\"args\":[1,true,\"args1\",\"@\"],\"account\":\"account1\"}", "account1", 1, true, "args1", "@"},
+	}
+
+	for _, table := range tables {
+		gotJson, err := GetBodyRequest(table.account, table.argsInt, table.argsBool, table.argsStr, table.argsSpecial)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if strings.Compare(table.wantJson, string(gotJson)) != 0 {
+			t.Errorf("Got: '%s', Want: '%s'", string(gotJson), table.wantJson)
 		}
 	}
 }
